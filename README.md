4756e7540ea428e665ca62221165faa1fec00056
Osama Maillal <osama.maillal@insa-rouen.fr>
2018-01-09 22:06:13 +0100

    authentification directe mise en place

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Pour faire tourner l'application, il faut installer Flask et lask-OAuthlib:
 
     $ pip install Flask-OAuthlib
 
+    $ pip install Flask-HTTPAuth
+
+    $ pip install Flask-SQLAlchemy
+
+    $ pip install passlib
+
 ## Exécution
 Ouvrir un terminal dans l'emplacement du repo et exécuter:
 
--- authentification.py
+++ authentification.py
@@ -1,11 +1,21 @@
-from flask import Flask, redirect, url_for, session, request, jsonify
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template
 from flask_oauthlib.client import OAuth
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from passlib.apps import custom_app_context as pwd_context
+import os
 
-
-app = Flask(__name__)
+# initialization
+app = Flask(__name__, static_url_path = "", static_folder = "templates")
 app.debug = True
-app.secret_key = 'casi'
+app.config['SECRET_KEY'] = 'casi'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 oauth = OAuth(app)
+
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
 
 linkedin = oauth.remote_app(
     'linkedin',
@@ -22,16 +32,68 @@ linkedin = oauth.remote_app(
     authorize_url='https://www.linkedin.com/uas/oauth2/authorization',
 )
 
+class User(db.Model):
+    __tablename__ = 'users'
+    email = db.Column(db.String(32), primary_key=True)
+    first_name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    password_hash = db.Column(db.Text)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+@auth.verify_password
+def verify_password(email, password):
+    # try to authenticate with username/password
+    user = User.query.filter_by(email=email).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return render_template('index.html',)
 
 
-@app.route('/login')
-def login():
+@app.route('/linkedinLogin')
+def linkedin_login():
     return linkedin.authorize(callback=url_for('authorized', _external=True))
 
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html',)
+
+
+@app.route('/directLogin')
+def direct_login():
+    return render_template('login.html',)
+
+@app.route('/successfulLogin', methods=['POST'])
+def successful_login():
+    email = request.form['email']
+    password = request.form['password']
+    user = User.query.filter_by(email=email).first()
+    if not user or not user.verify_password(password):
+        return redirect(url_for('direct_login'))
+    return redirect("http://prodageo.insa-rouen.fr/")
+
+@app.route('/successfulSignup', methods=['POST'])
+def successful_signup():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    user = User(email=email, first_name=first_name, last_name=last_name)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return redirect("http://prodageo.insa-rouen.fr/")
 
 @app.route('/logout')
 def logout():
@@ -71,4 +133,6 @@ linkedin.pre_request = change_linkedin_query
 
 
 if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
     app.run()
